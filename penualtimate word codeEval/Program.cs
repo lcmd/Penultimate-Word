a674d38b750008c730575483b70c4f7ee3373ed2
Your Name <your@example.com>
2016-05-26 10:45:51 -0400

    initial setup

---
+++ penualtimate word codeEval/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace penualtimate_word_codeEval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pull the 2nd to last word out
+            string line = "some line with text";
+
+             string[] word = line.Split();
+
+            Console.WriteLine(word[word.Length - 2]);
+
+            
+
+
+
+        }
+    }
+}
